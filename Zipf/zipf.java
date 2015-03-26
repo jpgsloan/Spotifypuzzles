9777b69f404507fdc465f823b773d3b460c473ab
jpgsloan <jpgsloan@gmail.com>
2015-03-25 18:33:19 -0700

    changes freq from int to double, cleans up code/comments. Fully working and complete now.

--- Zipf/zipf.java
+++ Zipf/zipf.java
@@ -5,101 +5,105 @@ import java.io.InputStreamReader;
 
 /* The zipf class is designed to act like an ArrayList<zipfNode> with 
  * a few extended features. It will automatically fill itself from standard input
- *  and, as well, it now contains a print method that prints the top m nodes. 
- *  The ArrayList is loaded up unordered to achieve constant time. Then sorted at
- *  O(nlogn) and unloaded at constant time.
+ *  and, as well, it contains a print method that prints the top m nodes (sorted by highest quality value).
+ *  The ArrayList is loaded up unordered at and then sorted at
+ *  O(n) + O(nlogn) -> O(nlogn). It is then printed to std output at O(m).
  *  */
-public class zipf {
+public class Zipf {
 	
-	ArrayList<zipfNode> zipfList;
-	int n,m;
-	boolean bool = true;
+	private ArrayList<ZipfNode> zipfList;
+	private int n,m;
+	private boolean readyToPrint = true;
 	
-	public zipf(){
-		this.zipfList = new ArrayList<zipfNode>(50000);
-		this.takeInput();
+	public Zipf(){
+		this.zipfList = new ArrayList<ZipfNode>();
+		this.loadListFromInput();
 	}
 	
-	public void takeInput(){ 
+	public void loadListFromInput(){
 		//loads the ArrayList with nodes created from input. The try-catch is used to assure
-		//the data is input correctly in a number of ways. It will just catch any exception
-		//and then exit.
+		//the input data correct in a number of ways. It will just catch any exception
+		//and then exit. It can easily be changed to be more specific with the types of errors thrown.
 		
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		
 		try {
-			String s = input.readLine();
-			int space = s.indexOf(' ');
-			this.n = Integer.parseInt(s.substring(0, space));
-			this.m = Integer.parseInt(s.substring(space + 1));
+			//Take in first line to set n and m
+			String newLine = input.readLine();
+			int spaceIndex = newLine.indexOf(' ');
+			this.n = Integer.parseInt(newLine.substring(0, spaceIndex));
+			this.m = Integer.parseInt(newLine.substring(spaceIndex + 1));
+			
 			//Check to make sure that n and m are within correct bounds.
 			if (this.m > this.n || this.m < 1){throw new Exception();}
 			if (this.n > 50000 || this.n < 1){throw new Exception();}
 			
 			//read in n lines.
-			int freq; String title = "Astro Lounge";
-			for (int n = 1; n <= this.n; n++) {				
-				s = input.readLine();
-				space = s.indexOf(' ');
-				freq = Integer.parseInt(s.substring(0, space));
-				title = s.substring(space + 1);
-				//Check that freq is in bounds and title is a-z,0-9,_ only between 1-30 characters.
+			Double freq;
+			String title = "";
+			for (int i = 1; i <= this.n; i++) {								
+				//Split line of input
+				newLine = input.readLine();
+				spaceIndex = newLine.indexOf(' ');
+				freq = Double.parseDouble(newLine.substring(0, spaceIndex));
+				title = newLine.substring(spaceIndex + 1);
+				
+                //Check that freq is in bounds and title is a-z,0-9,_ only between 1-30 characters.
 				if (freq < 0 || freq > Math.pow(10,12)){throw new Exception();}	
 				if (title.length() > 30 || !Pattern.matches("[a-z0-9_]+", title)){throw new Exception();}
+				
 				//make new node and add to arraylist.
-				zipfNode pikachu = new zipfNode(freq,n,title);
-				this.zipfList.add(pikachu);
+				ZipfNode newNode = new ZipfNode(freq,i,title);
+				this.zipfList.add(newNode);
 			}
+			
+			//Sorts the zipfList according to custom compareTo()
 			Collections.sort(this.zipfList);
 			
 		} catch (Throwable e){
-			this.bool = false;
+			this.readyToPrint = false;
+            System.out.println("input data is incorrect!");
 		}
 	}
 	
 	public void print(){
-		//Unloads m nodes of the tree to output. The stringbuilder creates one large
-		//string (of the top m songs) that is then printed all at once.
-		StringBuilder AllStar = new StringBuilder();
-		zipfNode onyx;
-		while (this.m != 1){
-			onyx = this.zipfList.remove(zipfList.size() - 1);
-			AllStar.append(onyx.title).append("\n");
-			this.m-=1;
+		//Unloads top m nodes of zipfList to output and prints to std output.
+		StringBuilder strBuilder = new StringBuilder();
+		ZipfNode node;
+        for (int i = 0; i < this.m; i++){
+            node = this.zipfList.get(this.zipfList.size() - i - 1);
+			strBuilder.append(node.title).append("\n");
 		}
-		onyx = this.zipfList.remove(zipfList.size() - 1);
-		AllStar.append(onyx.title);
-		System.out.print(AllStar.toString());
+		System.out.print(strBuilder.toString());
 	}
 	
 	public static void main(String[] args) {
-		zipf album = new zipf();
-		if (album.bool == true){
+		Zipf album = new Zipf();
+		if (album.readyToPrint == true){
 			album.print();
 		}
 	}
 	
 	/* A zipfNode is essentially a song from the album. Each node contains 
-	 * its track number(i), song title, and Zipf's Law adjusted play frequencies.
+	 * its track number (i), song title, and Zipf's Law adjusted play frequency (quality).
 	 */
-	private class zipfNode implements Comparable<zipfNode>{
+	private class ZipfNode implements Comparable<ZipfNode>{
 		private int i;
-		private int q;
+		private Double quality;
 		private String title = "";
 		
-		public zipfNode (int freq, int i, String song){
+		public ZipfNode (Double freq, int i, String song){
 			this.title = song;
 			this.i = i;
-			this.q = freq * i;
+			this.quality = freq * i;
 		}
 		
-		public int compareTo(zipfNode other) {
-			//compareTo is used to enforce the natural order so that
-			//Collections.sort() sorts the nodes by q value and when
-			// tied, by their tracklisting. 
-			if (other.q > this.q){
+		public int compareTo(ZipfNode other) {
+			// Sorts nodes by highest quality value and when
+			// tied, by lowest track number.
+			if (other.quality > this.quality){
 				return -1;
-			} else if (other.q < this.q){
+			} else if (other.quality < this.quality){
 				return 1;
 			} else {
 				if (other.i < this.i) {
